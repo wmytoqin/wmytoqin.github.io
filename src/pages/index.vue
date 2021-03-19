f7b0808d41825abcfe0bb2396cc293fde2d6ca88
SakuraLoo <suiiiiiiiiiiii@163.com>
2021-03-19 16:59:58 +0800

    init blank

---
+++ src/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+
+      index
+    
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: "index",
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/index.scss";
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import login from '@/pages/login'
-import home from '@/pages/home'
-import time from '@/pages/time'
-import hobby from '@/pages/hobby'
+import index from '@/pages/index'
 
 Vue.use(Router)
 
@@ -11,23 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'login',
-      component: login
-    },
-    {
-      path: '/home',
-      name: 'home',
-      component: home
-    },
-    {
-      path: '/time',
-      name: 'time',
-      component: time
-    },
-    {
-      path: '/hobby',
-      name: 'hobby',
-      component: hobby
+      name: 'index',
+      component: index
     }
   ]
 })
